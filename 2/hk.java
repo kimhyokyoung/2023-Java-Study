0e76582a4528787ebc372de5f0021c4acabec9a5
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-22 16:08:03 +0900

    Create hk.java

---
+++ 2/hk.java
@@ -0,0 +1,13 @@
+public class hyo_0522 {
+    public static void main(String[] args) {
+        Calculation cal = new Calculation();
+
+        int result1 = cal.plus(1,7);
+        int result2 = cal.minus(9,5);
+        int result3 = cal.multiplication(4,3);
+        int result4 = cal.division(9,3);
+
+        }
+    }
+
+z
