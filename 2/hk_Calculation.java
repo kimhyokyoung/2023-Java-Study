6ccfbb6b873159f25fee6abb6593036a73805a65
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-22 16:07:30 +0900

    Create hk_Calculation.java

---
+++ 2/hk_Calculation.java
@@ -0,0 +1,28 @@
+public class Calculation {
+    int plus(int a, int b) {
+        int result = a+b;
+        return result;
+        System.out.println("더하기 : "+result);
+    }
+    int minus(int a, int b) {
+        int result = a-b;
+        return result;
+        System.out.println("빼기 : "+result);
+    }
+
+    int multiplication(int a, int b) {
+        int result = a*b;
+        return result;
+        System.out.println("곱하기 : "+result);
+    }
+
+    int division(int a, int b) {
+        int result = a/b;
+        return result;
+        System.out.println("나누기 : "+result);
+
+    }
+
+
+}
+
