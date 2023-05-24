f52953f20c1985a06b8c673302e7879d4c47fed4
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-24 15:28:37 +0900

    ty_hantaeyoung.java

---
+++ 2/ty_hantaeyoung.java
@@ -0,0 +1,20 @@
+public class hantaeyoung {
+    int minus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    void standup() {
+        System.out.println("일어납니다.");
+    }
+
+    void sitdown() {
+        System.out.println("앉습니다.");
+
+    }
+
+    double divide(int x, int y) {
+        double result = (double) x / (double) y;
+        return result;
+    }
+}
