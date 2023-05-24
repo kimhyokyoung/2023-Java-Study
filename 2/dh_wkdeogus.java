8c86bad558425df13fe693d8156ea82877cfba4c
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-24 14:37:29 +0900

    dh_wkdeogus.java

---
+++ 2/dh_wkdeogus.java
@@ -0,0 +1,14 @@
+public class wkdeogus {
+
+    String university;
+    String department;
+
+    String schoolYear;
+
+    public wkdeogus(String university, String department, String schoolYear){
+            this.university = university;
+            this.department = department;
+            this.schoolYear = schoolYear;
+
+    }
+}
