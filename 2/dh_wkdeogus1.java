e85bbfaebb1c07bc4fc1a9b4e7f23e97e5a3d15b
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-24 14:37:46 +0900

    dh_wkdeogus1.java

---
+++ 2/dh_wkdeogus1.java
@@ -0,0 +1,9 @@
+public class wkdeogus1 {
+    public static void main(String[] args) {
+        wkdeogus w1 = new wkdeogus("삼육보건대학교", "의료정보과", "조교");
+
+        System.out.println("대학교 이름은 : " + w1.university);
+        System.out.println("학과는 : " + w1.department);
+        System.out.println("학년은 : " + w1.schoolYear);
+    }
+}
