a47ceee0ea126e90d42b29a826f5aab0148b29b8
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-29 00:18:47 +0900

    hk_hyo0529_01.java

---
+++ 3/hk_hyo0529_01.java
@@ -0,0 +1,11 @@
+public class hyo0529_01 {
+    public String age;
+    public String name;
+    public String color;
+
+    public hyo0529_01(String age, String name,String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+}
