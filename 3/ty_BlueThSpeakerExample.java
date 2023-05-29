b069accea65a85ac93d833e7c287a7cf93d13fe0
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-30 00:05:34 +0900

    ty_BlueThSpeakerExample.java

---
+++ 3/ty_BlueThSpeakerExample.java
@@ -0,0 +1,8 @@
+public class BlueThSpeakerExample {
+    public static void main(String[] args) {
+        BlueThSpeaker mySpeaker = new BlueThSpeaker("삼성 스피커","검정색");
+
+        System.out.println("모델:" + mySpeaker.model);
+        System.out.println("색상:" + mySpeaker.color);
+    }
+}
