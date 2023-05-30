6e8b5206a7711e736394a6f0d0f68b86445e15b4
HyoZzang <133212762+kimhyokyoung@users.noreply.github.com>
2023-05-31 00:10:21 +0900

    js_imStar.java

---
+++ 3/js_imStar.java
@@ -0,0 +1,11 @@
+public class imStar extends ImportStar {
+    public imStar(String a) {
+    super(a);
+    }
+
+    public static void main(String[] args) {
+        imStar star = new imStar("*");
+    }
+    }
+
+
